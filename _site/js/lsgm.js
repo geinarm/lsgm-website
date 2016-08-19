c5073fc746c6799a5623823ad8babef528248360
gmagnusson <gmagnusson@fc-md.umd.edu>
2016-08-19 00:30:00 -0400

    Initial commit with webpage draft

---
+++ _site/js/lsgm.js
@@ -0,0 +1,62 @@
+
+var lsgm = (function() {
+
+	function headerIntro() {
+	}
+
+	function showNav() {
+		$("#main-navbar").animate({"top":0}, 1000);
+	}
+
+	function showTitle() {
+		$(".test2").addClass("test");
+	}
+
+	function postRsvpIs() {
+		var values = {};
+		var fields = $( "#is-rsvp-form" ).serializeArray();
+		jQuery.each( fields, function( i, field ) {
+			values[field.name] = field.value;
+		});
+		console.log(values);
+
+		$.ajax({
+			url: "https://docs.google.com/forms/d/e/1FAIpQLSfwc3BjhjfcMEy9qTRuJZXAk9Zo74WVDtfhHvomwXP7tIWVkg/formResponse",
+			data: values,
+			type: "POST",
+			dataType: "xml",
+			statusCode: {
+				0: function() {
+					//Success
+				}
+			}
+		});
+	}
+
+	$( document ).ready(function() {
+	//$("#text-carousel-intro-section").hide()
+		//$("#text-carousel-intro-section").fadeIn(2000, showTitle);
+		$(".back-drop").delay(2000).fadeOut(3000);
+		$("#text-carousel-intro-section .caption").hide();
+		$("#text-carousel-intro-section .caption").delay(200).slideDown(1500);
+
+		//RSVP
+		$("#btn-rsvp-is").click(function() {
+			//postRsvpIs();
+		});
+		$("#btn-rsvp-us").click(function() {
+			
+		});
+
+		$(".google-maps .map-cover").click(function() {
+			console.log("Cover click");
+			$(".google-maps iframe").removeClass("scrolloff");
+			$(".google-maps .map-cover").fadeOut();
+		});
+		$(".google-maps iframe").mouseout(function() {
+			console.log("Map exit");
+			$(".google-maps .map-cover").fadeIn();
+		});
+	});
+
+})();
